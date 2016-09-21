abec64bd9212e96ac3b5d1a24aa6917d2233bb26
rdimartino <robert.dimartino@gmail.com>
2016-09-21 14:46:01 -0400

    Fixed tweet for new 538 layout

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def get_data(soup):  # Returns list of tuples with candidate probabilities
 # Tweet functions
 def tweet(data, timestamp):  # Post the tweet
     status = ('2016 US Pres. Election Chances!\n'
-              + ''.join(['{0} - {1}%\n'.format(format_name(d[0]), d[2]) for d in data])
+              + ''.join(['{0} - {1}%\n'.format(format_name(d[0]), d[1]) for d in data])
               + 'Updated: {:%b %d, %H:%M:%S} UTC\n'.format(timestamp)
               + url)
     try:
